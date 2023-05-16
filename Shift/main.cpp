437b0d3948315fc908c056bb5fe95326a68c66fe
Home <Home@DESKTOP-H7532SB>
2023-05-16 15:42:44 +0500

    Shift DONE

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -5,9 +5,12 @@ using std::cout;
 using std::endl;;
 
 #define tab "\t"
+//#define LEFT
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef LEFT
 	const int n = 10;
 	int arr[n] = { 0,1,2,3,4,5,6,7,8,9 };
 
@@ -21,7 +24,7 @@ void main()
 	//циклический сдвиг массива
 	int number_of_shifts;
 	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
-	for (int i = 0; i<number_of_shifts; i++)
+	for (int i = 0; i < number_of_shifts; i++)
 	{
 		int buffer = arr[0];
 		for (int i = 0; i < n; i++)
@@ -32,10 +35,43 @@ void main()
 	}
 	cout << endl;
 
-              //вывод сдвинутого массива на экран
+     //вывод сдвинутого массива на экран
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+#endif LEFT
+
+		const int n = 10;
+		int arr[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        //Вывод исходного массива на экран
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << tab;
+		}
+		cout << endl;
+
+		//Циклический сдвиг массива
+		int namber_of_shifts;
+		cout << "Введите количество сдвигов: "; cin >> namber_of_shifts;
+		for (int i = 0; i < namber_of_shifts; i++)
+		{
+			int buffer = arr[n - 1];
+			for (int i = n - 1; i > 0; i--)
+			{
+				arr[i] = arr[i - 1];
+			}
+			arr[0] = buffer;
+		}
+		cout << endl;
+
+		//Вывод сдвинутого массива на экран
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << tab;
+		}
+		cout << endl;
+
 }
